568453bfe3f88ef2b4c30a24751613a0c993262f
Kevin Macwan <kevin.macwan@ajackus.com>
2025-03-18 13:40:14 +0530

    refactor: added earlier user auth management abroach

--- src/core/react-query/mutations/authUser/loginMutation.js
+++ src/core/react-query/mutations/authUser/loginMutation.js
@@ -1,33 +1,42 @@
+import { useReducer } from 'react';
+import Config from 'react-native-config';
 import { useMutation } from '@tanstack/react-query';
+
+import reducer, { initialState } from '../../../state/authuser/authuserReducer';
 import httpService from '../../../services/httpService';
 import oauthService from '../../../services/oauthService';
-import Config from 'react-native-config';
+import authuserActions from '../../../state/authuser/authuserActions';
 
 export const useLoginMutation = displayAlert => {
+  const [dispatch] = useReducer(reducer, initialState || {});
+
   return useMutation({
     mutationFn: async loginData => {
+      dispatch(authuserActions.login(loginData));
       const token = await oauthService.authenticateWithPasswordFlow(loginData);
       oauthService.setAccessToken(token.data);
 
-      const oauthuser = httpService.makeRequest(
+      const oauthuser = await httpService.makeRequest(
         'get',
         `${Config.API_URL}coreuser/me/`,
       );
 
-      oauthService.setOauthUser(oauthuser, oauthuser);
+      oauthService.setOauthUser(oauthuser);
 
-      const coreuser = httpService.makeRequest(
+      const coreuser = await httpService.makeRequest(
         'get',
         `${Config.API_URL}coreuser/`,
       );
 
-      oauthService.setCurrentCoreUser(oauthuser, coreuser);
+      oauthService.setCurrentCoreUser(coreuser, oauthuser);
+      return oauthuser;
     },
     onSuccess: data => {
-      oauthService.setAccessToken(token.data);
+      dispatch(authuserActions.loginSuccess(data));
       displayAlert('success', 'Sign in successful');
     },
     onError: err => {
+      dispatch(authuserActions.loginFail(err));
       displayAlert('error', 'Sign in failed');
     },
   });
